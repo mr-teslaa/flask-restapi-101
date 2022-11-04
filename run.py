e9a9f2b4e4692e93c74149397f7a04e43628725c
mr-teslaa <mr.teslaa@protonmail.com>
2022-11-04 12:27:48 +0600

    finish section-4 lesson-6. understanding basic api status code

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from re import A
 from flask import Flask
 from flask_restful import Resource
 from flask_restful import Api
@@ -10,14 +9,25 @@ api = Api(app)
 # RESOURCE is all about what you are providing through api
 # this is a resource, in our case our resources is STUDENTS
 
-class Student(Resource):
-    def get(self, name):
-        return {"student": name}
+items = []
 
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        
+        return {'item': None}, 404    # 404 is the data is not found in the server
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201       # 201 status code is for successfult post method
+    
 # we don't need to define the route differently when we are using api
 # instead we should define that in this way, after the adding resouces we need to
 # tell the route
-api.add_resource(Student, '/student/<string:name>') # localhost/student/Jhon
+api.add_resource(Item, '/item/<string:name>') # localhost/student/Jhon
 
 
 app.run()
